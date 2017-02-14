c92d9fc480e3a7efdd1ebc3554192c83d5b96f99
Hussein Ait-Lahcen <hussein.aitlahcen@gmail.com>
2017-02-14 20:26:29 +0100

    added assets

--- src/app.js
+++ src/app.js
@@ -1,27 +1,16 @@
-
 var HelloWorldLayer = cc.Layer.extend({
     sprite: null,
     ctor: function () {
-        //////////////////////////////
-        // 1. super init first
         this._super();
 
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask the window size
         var size = cc.winSize;
 
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-        var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
-        // position the label on the center of the screen
-        helloLabel.x = size.width / 2;
-        helloLabel.y = size.height / 2 + 200;
-        // add the label as a child to this layer
-        this.addChild(helloLabel, 5);
+        var ufo = new cc.Sprite(res.assets.game.ship.ufo.yellow);
+        ufo.setScale(0.5, 0.5);
+        ufo.setPosition(new cc.Point(size.width / 2, size.height / 2));
+
+        this.addChild(ufo, 5);
+
         return true;
     }
 });
@@ -33,4 +22,3 @@ var HelloWorldScene = cc.Scene.extend({
         this.addChild(layer);
     }
 });
-
--- src/resource.js
+++ src/resource.js
@@ -1,8 +1,56 @@
-var res = {
+if (!String.format) {
+    String.format = function (format) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        return format.replace(/{(\d+)}/g, function (match, number) {
+            return typeof args[number] != 'undefined' ?
+                args[number] :
+                match;
+        });
+    };
+}
 
+var res = {
+    assets: {
+        background: {
+            black: "res/assets/background/black.png",
+            blue: "res/assets/background/blue.png",
+            dark_purple: "res/assets/background/darkPurple.png",
+            purple: "res/assets/background/purple.png",
+        },
+        game: {
+            ship: {
+                type_0: {
+                    blue: "res/assets/game/ship/00.png",
+                    green: "res/assets/game/ship/10.png",
+                    orange: "res/assets/game/ship/20.png",
+                    red: "res/assets/game/ship/30.png"
+                },
+                type_1: {
+                    blue: "res/assets/game/ship/01.png",
+                    green: "res/assets/game/ship/11.png",
+                    orange: "res/assets/game/ship/21.png",
+                    red: "res/assets/game/ship/31.png"
+                },
+                type_2: {
+                    blue: "res/assets/game/ship/02.png",
+                    green: "res/assets/game/ship/12.png",
+                    orange: "res/assets/game/ship/22.png",
+                    red: "res/assets/game/ship/32.png"
+                },
+                ufo: {
+                    blue: "res/assets/game/ship/40.png",
+                    green: "res/assets/game/ship/41.png",
+                    red: "res/assets/game/ship/42.png",
+                    yellow: "res/assets/game/ship/43.png"
+                }
+            }
+        }
+    }
 };
 
-var g_resources = [];
-for (var i in res) {
-    g_resources.push(res[i]);
-}
+var g_resources = [
+    res.assets.background.black,
+    res.assets.background.blue,
+    res.assets.background.dark_purple,
+    res.assets.background.purple
+];
